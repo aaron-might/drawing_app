bb81539b5d38d9c8f4240b7bbe97a378fc7c5d5c
aaron-might <aaronmighty7@gmail.com>
2021-09-17 17:05:55 -0400

    write the drawline function

--- script.js
+++ script.js
@@ -7,25 +7,31 @@ const colorEl = document.getElementById('color');
 const ctx = canvas.getContext('2d');
 
 let size = 30;
-// let x = 50;
-// let y = 50;
+let x = undefined;
+let y = undefined;
+
 let isPressed=false;
 let color ="black"
 
 canvas.addEventListener('mousedown', ()=> {
     isPressed = true;
+
+    x = undefined;
+    y = undefined;
 });
 
-canvas.addEventListener('mousedown', ()=> {
-    isPressed = true;
-});
+// canvas.addEventListener('mousedown', ()=> {
+//     isPressed = true;
+// });
 
 canvas.addEventListener('mousemove', (e) => {
     if(isPressed){    
-        const x = e.offsetX;
-        const y = e.offsetY;
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
         
-        drawCircle(x,y);
+        drawLine(x, y, x2, y2);
+        x = x2; 
+        y = y2;
     }    
 
 console.log(e);
@@ -37,7 +43,16 @@ function drawCircle(x,y) {
     ctx.fill();
     ctx.fillStyle = color;
 }
+drawLine(100,100, 200, 200)
 
+function drawLine (x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+  
+}
 
 increaseBtn.addEventListener("click",() =>{
     size += 5;
